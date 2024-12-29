338d0ec03fef0f26c674eb442ca88d4d41280ec7
Sebastian Bortz <sebastianbortz@gmail.com>
2024-12-29 16:24:08 +0100

    Added more test coverage

--- Shopping.Cart/Infrastructure/IKafkaPublisher.cs
+++ Shopping.Cart/Infrastructure/IKafkaPublisher.cs
@@ -4,17 +4,3 @@ public interface IKafkaPublisher
 {
     public Task PublishAsync(string topic, object @event);
 }
-
-public class FakeKafkaPublisher : IKafkaPublisher
-{
-    public async Task PublishAsync(string topic, object @event)
-    {
-        Random random = new Random();
-        double value = random.NextDouble();
-
-        if (value < 0.5)
-        {
-            throw new Exception("Publish failed");
-        }
-    }
-}
